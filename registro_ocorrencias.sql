83724f29fc75d9261a6e70966dfab285bdc981a8
EA - Gabrielly Gonçalves Custódio <gabrielly_goncalves@educadventista.org>
2025-05-26 11:20:14 -0300

    26/05

--- registro_ocorrencias.sql
+++ registro_ocorrencias.sql
@@ -2,16 +2,16 @@ CREATE DATABASE IF NOT EXISTS registro_ocorrencias;
 
 USE registro_ocorrencias;
 
-CREATE TABLE ocorrencias (
+CREATE TABLE IF NOT EXISTS ocorrencias (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    aluno VARCHAR(100) NOT NULL,
+    aluno VARCHAR(255) NOT NULL,
     data DATE NOT NULL,
     descricao TEXT NOT NULL,
-    tipo VARCHAR(20) DEFAULT 'simples'
+    tipo ENUM('simples', 'grave') NOT NULL DEFAULT 'simples'
 );
 
-CREATE TABLE ocorrencias_graves (
+CREATE TABLE IF NOT EXISTS ocorrencias_graves (
     id INT PRIMARY KEY,
     nivel_gravidade ENUM('Baixa', 'Média', 'Alta') NOT NULL,
-    FOREIGN KEY (id) REFERENCES ocorrencias(id)
+    FOREIGN KEY (id) REFERENCES ocorrencias(id) ON DELETE CASCADE
 );
